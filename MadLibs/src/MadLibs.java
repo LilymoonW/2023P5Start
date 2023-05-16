8ab81314e80bb3018d769ecc5e87b7d6870c2d5d
s-LWHALEN <s-LWHALEN@484E293879.lwsd.k12wa.org>
2023-05-16 12:12:20 -0700

    Added the main control Loop

--- MadLibs/src/MadLibs.java
+++ MadLibs/src/MadLibs.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-/**Plays  a game of Madlibs
+/**
+ * Plays a game of Madlibs
  * 
  * @author s-LWHALEN
  *
@@ -14,9 +15,29 @@ public class MadLibs {
 		System.out.println("and phrases to fill in a story.");
 		System.out.println("The result will be inputed to an output file.");
 		System.out.println("");
-		
-		//creates scanner to read from the console
+
+		// creates scanner to read from the console
 		Scanner console = new Scanner(System.in);
+
+		// main game loop until user quits
+		boolean done = false;
+		do {
+			System.out.print("(C)reate a mad-lib, (V)iew mad-lib, (Q)uit?");
+			String command = console.nextLine();
+			switch (command.toUpperCase()) {
+			case "C":
+				break;
+			case "V":
+				break;
+			case "Q":
+				done = true;
+				break;
+			default:
+				System.out.println("Command" + command + " is not valid!");
+				break;
+
+			}
+		} while (!done);
 
 	}
 
