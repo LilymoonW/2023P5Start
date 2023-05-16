f5b50f7f3183c7a986bd0ec89a0b60f5abb49b9b
s-LWHALEN <s-LWHALEN@484E293879.lwsd.k12wa.org>
2023-05-16 12:04:42 -0700

    Made intro text

---
+++ MadLibs/src/MadLibs.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**Plays  a game of Madlibs
+ * 
+ * @author s-LWHALEN
+ *
+ */
+public class MadLibs {
+
+	public static void main(String[] args) {
+		// intro text
+		System.out.println("Welcome to the game of Mad Libs.");
+		System.out.println("I will ask you to provide various words");
+		System.out.println("and phrases to fill in a story.");
+		System.out.println("The result will be inputed to an output file.");
+		System.out.println("");
+		
+		//creates scanner to read from the console
+		Scanner console = new Scanner(System.in);
+
+	}
+
+}
