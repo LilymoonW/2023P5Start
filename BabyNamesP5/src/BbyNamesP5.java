f61168d574d6f2fb5b308b5852e88b52dc8d81c7
s-LWHALEN <s-LWHALEN@484E293879.lwsd.k12wa.org>
2023-05-25 11:41:15 -0700

    Babynames created with messge

---
+++ BabyNamesP5/src/BbyNamesP5.java
@@ -0,0 +1,14 @@
+/**Graphs a given names popularity by decade from 1890 to 2010
+ * 
+ * @author s-LWHALEN
+ *
+ */
+public class BbyNamesP5 {
+	public static void main(String[] args) {
+		System.out.println("This program searches through the Social");
+		System.out.println("Security Service Top name from 1890 to 2010");
+		System.out.println("It then graphs the popularity of the name.");
+		System.out.println();
+
+	}
+}
